635a88fc2855559f463b1328645667344ba7d328
cyotee <cyotee@gmail.com>
2016-04-13 15:04:09 -0700

    corrected registry name in build script

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 echo "Building docker.io/cyotee/jbpm-ent"
 docker build -t docker.io/cyotee/jbpm-ent .
 echo "Pushing docker.io/cyotee/jbpm-ent"
-docker push ocker.io/cyotee/jbpm-ent
+docker push docker.io/cyotee/jbpm-ent
